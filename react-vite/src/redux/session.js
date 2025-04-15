98dd89162cb3b035b3428312bfb6350010115bd4
danehoov <DaneHoov22@gmail.com>
2025-04-15 16:44:34 -0600

    Add/Remove stocks to/from watchlist and portfolio. thunk, state, reducers added for authenticated user. imports added to store.js

--- react-vite/src/redux/session.js
+++ react-vite/src/redux/session.js
@@ -6,124 +6,124 @@ const ADD_TO_PORTFOLIO = "portfolio/addToPortfolio";
 const REMOVE_FROM_PORTFOLIO = "portfolio/removeFromPortfolio";
 
 const setUser = (user) => ({
-  type: SET_USER,
-  payload: user,
+    type: SET_USER,
+    payload: user,
 });
 
 const removeUser = () => ({
-  type: REMOVE_USER,
+    type: REMOVE_USER,
 });
 
 const addToWatchlist = (stock) => ({
-  type: ADD_TO_WATCHLIST,
-  payload: stock,
+    type: ADD_TO_WATCHLIST,
+    payload: stock,
 });
 const removeFromWatchlist = (stock) => ({
-  type: REMOVE_FROM_WATCHLIST,
-  payload: stock,
+    type: REMOVE_FROM_WATCHLIST,
+    payload: stock,
 });
 
 const addToPortfolio = (stock) => ({
-  type: ADD_TO_PORTFOLIO,
-  payload: stock,
+    type: ADD_TO_PORTFOLIO,
+    payload: stock,
 });
 
 const removeFromPortfolio = (stockSymbol) => ({
-  type: REMOVE_FROM_PORTFOLIO,
-  payload: stockSymbol,
+    type: REMOVE_FROM_PORTFOLIO,
+    payload: stockSymbol,
 });
 
 export const thunkAuthenticate = () => async (dispatch) => {
-  const response = await fetch("/api/auth/");
-  if (response.ok) {
-    const data = await response.json();
-    if (data.errors) {
-      return;
-    }
+    const response = await fetch("/api/auth/");
+    if (response.ok) {
+        const data = await response.json();
+        if (data.errors) {
+            return;
+        }
 
-    dispatch(setUser(data));
-  }
+        dispatch(setUser(data));
+    }
 };
 
 export const thunkLogin = (credentials) => async (dispatch) => {
-  const response = await fetch("/api/auth/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(credentials),
-  });
+    const response = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+    });
 
-  if (response.ok) {
-    const data = await response.json();
-    dispatch(setUser(data));
-  } else if (response.status < 500) {
-    const errorMessages = await response.json();
-    return errorMessages;
-  } else {
-    return { server: "Something went wrong. Please try again" };
-  }
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(setUser(data));
+    } else if (response.status < 500) {
+        const errorMessages = await response.json();
+        return errorMessages;
+    } else {
+        return { server: "Something went wrong. Please try again" };
+    }
 };
 
 export const thunkSignup = (user) => async (dispatch) => {
-  const response = await fetch("/api/auth/signup", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(user),
-  });
+    const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+    });
 
-  if (response.ok) {
-    const data = await response.json();
-    dispatch(setUser(data));
-  } else if (response.status < 500) {
-    const errorMessages = await response.json();
-    return errorMessages;
-  } else {
-    return { server: "Something went wrong. Please try again" };
-  }
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(setUser(data));
+    } else if (response.status < 500) {
+        const errorMessages = await response.json();
+        return errorMessages;
+    } else {
+        return { server: "Something went wrong. Please try again" };
+    }
 };
 
 export const thunkLogout = () => async (dispatch) => {
-  await fetch("/api/auth/logout");
-  dispatch(removeUser());
+    await fetch("/api/auth/logout");
+    dispatch(removeUser());
 };
 
 export const thunkAddToWatchlist = (stock) => async (dispatch) => {
-  const response = await fetch("/api/stocks/watchlist", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(stock),
-  });
-  if (response.ok) {
-    dispatch(addToWatchlist(stock));
-  }
+    const response = await fetch("/api/stocks/watchlist", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(stock),
+    });
+    if (response.ok) {
+        dispatch(addToWatchlist(stock));
+    }
 };
 
 export const thunkRemoveFromWatchlist = (stockSymbol) => async (dispatch) => {
-  const response = await fetch(`/api/stocks/watchlist/${stockSymbol}`, {
-    method: "DELETE",
-  });
-  if (response.ok) {
-    dispatch(removeFromWatchlist(stockSymbol));
-  }
+    const response = await fetch(`/api/stocks/watchlist/${stockSymbol}`, {
+        method: "DELETE",
+    });
+    if (response.ok) {
+        dispatch(removeFromWatchlist(stockSymbol));
+    }
 };
 
 export const thunkAddToPortfolio = (stock) => async (dispatch) => {
-  const response = await fetch("/api/stocks/portfolio", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(stock),
-  });
-  if (response.ok) {
-    dispatch(addToPortfolio(stock));
-  }
+    const response = await fetch("/api/stocks/portfolio", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(stock),
+    });
+    if (response.ok) {
+        dispatch(addToPortfolio(stock));
+    }
 };
 
 export const thunkRemoveFromPortfolio = (stockSymbol) => async (dispatch) => {
-  const response = await fetch(`/api/stocks/portfolio/${stockSymbol}`, {
-    method: "DELETE",
-  });
-  if (response.ok) {
-    dispatch(removeFromPortfolio(stockSymbol));
-  }
+    const response = await fetch(`/api/stocks/portfolio/${stockSymbol}`, {
+        method: "DELETE",
+    });
+    if (response.ok) {
+        dispatch(removeFromPortfolio(stockSymbol));
+    }
 };
 
 const sessionInitialState = { user: null };
@@ -131,34 +131,34 @@ const watchlistInitialState = [];
 const portfolioInitialState = [];
 
 function sessionReducer(state = sessionInitialState, action) {
-  switch (action.type) {
-    case SET_USER:
-      return { ...state, user: action.payload };
-    case REMOVE_USER:
-      return { ...state, user: null };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case SET_USER:
+            return { ...state, user: action.payload };
+        case REMOVE_USER:
+            return { ...state, user: null };
+        default:
+            return state;
+    }
 }
 
 function watchlistReducer(state = watchlistInitialState, action) {
-  switch (action.type) {
-    case ADD_TO_WATCHLIST:
-      return [...state, action.payload];
-    case REMOVE_FROM_WATCHLIST:
-      return state.filter((stock) => stock.symbol !== action.payload);
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case ADD_TO_WATCHLIST:
+            return [...state, action.payload];
+        case REMOVE_FROM_WATCHLIST:
+            return state.filter((stock) => stock.symbol !== action.payload);
+        default:
+            return state;
+    }
 }
 
 function portfolioReducer(state = portfolioInitialState, action) {
-  switch (action.type) {
-    case ADD_TO_PORTFOLIO:
-      return [...state, action.payload];
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case ADD_TO_PORTFOLIO:
+            return [...state, action.payload];
+        default:
+            return state;
+    }
 }
 
 export { sessionReducer, watchlistReducer, portfolioReducer };

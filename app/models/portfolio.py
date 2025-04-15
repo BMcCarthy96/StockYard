63fec200542cb3ed17433096ef79a69bfc6a26ca
Brandon McCarthy <Bmac96.dev@gmail.com>
2025-04-14 20:11:11 -0400

    Modified login styling to match signup

--- app/models/portfolio.py
+++ app/models/portfolio.py
@@ -7,11 +7,14 @@ class Portfolio(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False, unique=True)
-    cash_balance = db.Column(db.Float, default=0.0)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    balance = db.Column(db.Float, nullable=False, default=0.0)
+
+    user = db.relationship('User', back_populates='portfolios')
+
     def to_dict(self):
         return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "balance": self.balance
+            'id': self.id,
+            'user_id': self.user_id,
+            'balance': self.balance,
         }

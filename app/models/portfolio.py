d0c62ff776db95a4cf56dc6cbc0d9edd389cfc9d
Brandon McCarthy <Bmac96.dev@gmail.com>
2025-04-16 21:32:44 -0400

    Modified portfolio.py

--- app/models/portfolio.py
+++ app/models/portfolio.py
@@ -1,159 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 from .db import db, environment, SCHEMA, add_prefix_for_prod
-=======
-<<<<<<< HEAD
-=======
-from .db import db
->>>>>>> bdf008d (Modified login styling to match signup)
-=======
-=======
->>>>>>> 6061f4a (Modified login styling to match signup)
-=======
->>>>>>> b4aa7a1 (Added portfolio)
-<<<<<<< HEAD
-=======
-from .db import db
-=======
-from .db import db, environment, SCHEMA, add_prefix_for_prod
->>>>>>> 2268bc7 (Rebased)
-<<<<<<< HEAD
->>>>>>> 94b375f (Rebased)
-=======
-=======
-from .db import db, environment, SCHEMA, add_prefix_for_prod
-=======
-from .db import db
->>>>>>> ef081c6 (Modified login styling to match signup)
->>>>>>> 73157a9 (Modified login styling to match signup)
-<<<<<<< HEAD
->>>>>>> 6061f4a (Modified login styling to match signup)
-=======
-=======
-from .db import db, environment, SCHEMA, add_prefix_for_prod
->>>>>>> 074b939 (Added portfolio)
->>>>>>> b4aa7a1 (Added portfolio)
 
 class Portfolio(db.Model):
     __tablename__ = 'portfolios'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6061f4a (Modified login styling to match signup)
-=======
->>>>>>> b4aa7a1 (Added portfolio)
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-<<<<<<< HEAD
-<<<<<<< HEAD
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False, unique=True)
-    cash_balance = db.Column(db.Float, default=0.0)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 572533a (migration and jsx done)
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "balance": self.balance
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 6061f4a (Modified login styling to match signup)
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     balance = db.Column(db.Float, nullable=False, default=0.0)
-
-    user = db.relationship('User', back_populates='portfolios')
-<<<<<<< HEAD
->>>>>>> bdf008d (Modified login styling to match signup)
-=======
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
-=======
-
-    user = db.relationship("User", back_populates="portfolio")
->>>>>>> b4aa7a1 (Added portfolio)
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False, unique=True)
-    cash_balance = db.Column(db.Float, default=0.0)
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-    user = db.relationship("User", back_populates="portfolio")
->>>>>>> 94b375f (Rebased)
-
-=======
->>>>>>> 5fdda3a (Rebased)
-=======
-
->>>>>>> 6061f4a (Modified login styling to match signup)
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'user_id': self.user_id,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            'cash_balance': self.cash_balance
->>>>>>> d807041 (added to models)
-=======
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "balance": self.balance
->>>>>>> 9a957cc (updated seeders and other files)
-        }
-=======
-            'balance': self.balance,
-=======
-            'cash_balance': self.cash_balance
->>>>>>> 94b375f (Rebased)
-=======
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "balance": self.balance
->>>>>>> e5b7873 (updated seeders and other files)
-=======
-            'balance': self.balance,
->>>>>>> ef081c6 (Modified login styling to match signup)
->>>>>>> 6061f4a (Modified login styling to match signup)
-=======
-            'cash_balance': self.cash_balance
->>>>>>> b4aa7a1 (Added portfolio)
-=======
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "balance": self.balance
->>>>>>> c0162d5 (updated seeders and other files)
-=======
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    balance = db.Column(db.Float, nullable=False, default=0.0)
-=======
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False, unique=True)
-    cash_balance = db.Column(db.Float, default=0.0)
->>>>>>> aa882ee (Rebased)
 
     user = db.relationship("User", back_populates="portfolio")
 
@@ -161,19 +16,5 @@ class Portfolio(db.Model):
         return {
             'id': self.id,
             'user_id': self.user_id,
-<<<<<<< HEAD
-            'balance': self.balance,
->>>>>>> 1eee459 (Modified login styling to match signup)
-=======
-            'cash_balance': self.cash_balance
->>>>>>> aa882ee (Rebased)
-=======
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "balance": self.balance
->>>>>>> c53b576 (updated seeders and other files)
+            'balance': self.balance
         }
->>>>>>> 30411c3 (Modified login styling to match signup)
->>>>>>> bdf008d (Modified login styling to match signup)

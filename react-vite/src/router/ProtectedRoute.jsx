bce333a69992e112be37bfbe8a5270c83d53b270
Brandon McCarthy <Bmac96.dev@gmail.com>
2026-08-28 21:44:58 -0400

    Serve the StockYard entry on Vercel

--- react-vite/src/router/ProtectedRoute.jsx
+++ react-vite/src/router/ProtectedRoute.jsx
@@ -2,8 +2,12 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 export default function ProtectedRoute() {
-  const sessionUser = useSelector((state) => state.session.user);
+  const { user: sessionUser, loaded } = useSelector((state) => state.session);
   const location = useLocation();
+
+  if (!loaded) {
+    return <div className="page-loading">Checking your session...</div>;
+  }
 
   if (!sessionUser) {
     return <Navigate to="/login" replace state={{ from: location }} />;
